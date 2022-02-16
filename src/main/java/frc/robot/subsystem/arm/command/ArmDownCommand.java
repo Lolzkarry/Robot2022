3dc95b076836131496bc781b0b094ef0f1305473
Lolzkarry <46729989+Lolzkarry@users.noreply.github.com>
2022-02-16 16:48:05 -0600

    de

--- src/main/java/frc/robot/subsystem/arm/command/ArmDownCommand.java
+++ src/main/java/frc/robot/subsystem/arm/command/ArmDownCommand.java
@@ -1,4 +1,20 @@
 package frc.robot.subsystem.arm.command;
 
-public class ArmDownCommand {
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystem.arm.Arm;
+
+public class ArmDownCommand extends CommandBase {
+    private Arm arm;
+    private double armAngle;
+    public ArmDownCommand(Arm arm, double armAngle) {
+        this.arm = arm;
+        this.armAngle = armAngle;
+        addRequirements(arm);
+    }
+    @Override
+    public void initialize() {
+        arm.setAngle(armAngle);
+    }
+
 }
---
+++ src/main/java/frc/robot/subsystem/arm/command/ArmUpCommand.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystem.arm.command;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystem.arm.Arm;
+
+public class ArmUpCommand extends CommandBase {
+    private Arm arm;
+    private double armAngle;
+}
